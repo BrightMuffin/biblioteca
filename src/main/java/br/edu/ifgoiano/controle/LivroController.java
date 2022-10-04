09bb958567336fbaab80d7bfc4eadc60b9c057ce
2020108102540060 <2020108102540060@lab02-29.ifgoiano.local>
2022-10-04 08:46:27 -0300

    0846

--- src/main/java/br/edu/ifgoiano/controle/LivroController.java
+++ src/main/java/br/edu/ifgoiano/controle/LivroController.java
@@ -3,6 +3,7 @@ package br.edu.ifgoiano.controle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.edu.ifgoiano.entidade.Livro;
 import br.edu.ifgoiano.servico.LivroService;
@@ -29,6 +30,13 @@ public class LivroController{
 		model.addAttribute("livro", livro);
 		
 		return "inserir-livro";
+	}
+	
+	@PostMapping("/livros/inserir")
+	public String inserirLivro(Livro livro) {
+		//inserir livro no banco de dados
+		
+		return "redirect:/livros";
 	}
 
 }
